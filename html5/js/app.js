92768a6e006b9f3714ba793204e1779d837c1308
@javaset <vla78dan@gmail.ru>
2024-12-01 10:40:15 +0300

    18-38

--- html5/js/app.js
+++ html5/js/app.js
@@ -1,4 +1,12 @@
 const swiperText = new Swiper('.swiper', {
     speed: 1700,
-    mousewheel: { },
+    mousewheel: {},
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
 })
